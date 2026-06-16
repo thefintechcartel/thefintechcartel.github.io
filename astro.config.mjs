fadd6f0548e0dbda616824e9699f78698cf62dcc
Advik Jain <advikjain@Adviks-MacBook-Air.local>
2026-06-16 07:11:01 +0530

    wow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
+  site: 'https://thefintechcartel.github.io',
   // If deploying to yourusername.github.io (not a project page), base is '/'
   // If deploying to yourusername.github.io/tfc, set: base: '/tfc'
 });
